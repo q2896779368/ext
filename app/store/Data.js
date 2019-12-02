d45191e4309aa4ba83fd1b5b0d39e3efd63a1650
马宏涛 <2896779368@qq.com>
2019-12-02 11:44:11 +0800

    ext

--- app/store/Data.js
+++
@@ -1,8 +0,0 @@
-/**
- * 各元件库属性Store（数据集）
- */
-Ext.define('Cosmo.store.Data', {
-    extend: 'Ext.data.Store',
-
-    alias: 'store.Data'
-});
--- app/util/CosmoTree.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Cosmo.util.CosmoTree',{
-    extend: 'Ext.tree.Panel',
-
-    alias: "widget.cosmotree",
-
-    alternateClassName: ['Cosmo.util.CosmoTree','Cosmo.CosmoTree'],
-
-    mixins:[ 'Cosmo.util.TreeFilter']
-});
